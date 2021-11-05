9e3f2379c3559e0a361a68582994f45568cd1196
Pedro <pedro.oficial1969@gmail.com>
2021-11-05 18:51:33 -0300

    adiciona requisito 3 resolvido

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,6 +12,7 @@ class Form extends Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      /* hasTrunfo, */
       isSaveButtonDisabled,
       onInputChange,
       onSaveButtonClick,
@@ -145,6 +146,7 @@ Form.propTypes = {
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
+  /*  hasTrunfo: PropTypes.bool.isRequired, */
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
